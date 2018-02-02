53e7330d1b3820c7bb95893e1033f9c7dd395410
pperreiah <31325721+pperreiah@users.noreply.github.com>
2018-02-02 11:24:49 -0500

    Update Article.js

--- models/Article.js
+++ models/Article.js
@@ -6,7 +6,8 @@ var ArticleSchema = new Schema({
   // title is a required string
   title: {
     type: String,
-    required: true
+    required: true,
+    unique: true
   },
   link: {
     type: String,
